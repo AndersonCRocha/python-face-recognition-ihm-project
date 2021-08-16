46449fbd401528823d209a185a189ced714230b8
AndersonCRocha <dan.sjose20@gmail.com>
2021-08-16 19:40:25 -0300

    Criação do banner e configuração de run do projeto

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from src.environment import Environment
+from src.logger import Logger
+
 if __name__ == '__main__':
-    print('Condomínio inteligente - Anderson Rocha')
+    Logger.debug = True
+    Logger.banner('./resources/banner.txt')
+    environment = Environment()
+    environment.run()
