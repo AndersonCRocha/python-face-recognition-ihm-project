d1a6f26bea437e2aa6656d5d54d26a72300a7193
AndersonCRocha <dan.sjose20@gmail.com>
2021-08-17 21:42:34 -0300

    Criação de arquivo de utilitários e inclusão de algumas constantes

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,15 @@
 import pathlib
 from os import path
 
+CONFIDENCE_LEVEL = 0.7
+
+TIME_BETWEEN_PEOPLE_ARRIVAL = 20
+TIME_BETWEEN_PEOPLE_LEAVING = 28
 
 PROJECT_PATH = pathlib.Path().resolve()
-BANNER_FILE_PATH = path.join(PROJECT_PATH, 'resources', 'banner.txt')
-SETTINGS_FILE_PATH = path.join(PROJECT_PATH, 'resources', 'settings.json')
+RESOURCES_PATH = path.join(PROJECT_PATH, 'resources')
+PHOTOS_PATH = path.join(RESOURCES_PATH, 'photos')
+BANNER_FILE_PATH = path.join(RESOURCES_PATH, 'banner.txt')
+SETTINGS_FILE_PATH = path.join(RESOURCES_PATH, 'settings.json')
+
+
---
+++ src/utils.py
@@ -0,0 +1,105 @@
+import random
+from os import path
+from time import localtime, strftime
+
+import face_recognition
+
+from src.constants import PHOTOS_PATH
+
+PEOPLE = [
+    # Tenant
+    "john-01.jpg",
+    "john-02.jpg",
+    "john-03.jpg",
+
+    # Visitor
+    "grace-01.jpeg",
+    "grace-02.jpeg",
+    "grace-03.jpeg",
+
+    # Tenant
+    "mary-01.jpg",
+    "mary-02.jpg",
+    "mary-03.jpg",
+
+    # Visitor
+    "phillip-01.jpeg",
+    "phillip-02.jpeg",
+    "phillip-03.jpeg",
+
+    # Tenant
+    "joseph-01.jpg",
+    "joseph-02.jpg",
+    "joseph-03.jpg",
+]
+
+# PEOPLE = [
+#     # Tenant
+#     "john-01.jpg",
+#     "john-02.jpg",
+#     "john-03.jpg",
+#
+#     # Visitor
+#     "paul-01.jpg",
+#     "paul-02.jpg",
+#     "paul-03.jpg",
+#
+#     # Visitor
+#     "grace-01.jpg",
+#     "grace-02.jpg",
+#     "grace-03.jpg",
+#
+#     # Tenant
+#     "mary-01.jpg",
+#     "mary-02.jpg",
+#     "mary-03.jpg",
+#
+#     # Visitor
+#     "phillip-01.jpg",
+#     "phillip-02.jpg",
+#     "phillip-03.jpg",
+#
+#     # Visitor
+#     "jane-01.jpg",
+#     "jane-02.jpg",
+#     "jane-03.jpg",
+#
+#     # Visitor
+#     "matheo-01.jpg",
+#     "matheo-02.jpg",
+#     "matheo-03.jpg",
+#
+#     # Tenant
+#     "joseph-01.jpg",
+#     "joseph-02.jpg",
+#     "joseph-03.jpg",
+#
+#     # Visitor
+#     "claire-01.jpg",
+#     "claire-02.jpg",
+#     "claire-03.jpg"
+# ]
+
+
+def generate_log_filename():
+    current_date = strftime("%d-%m-%Y_%H-%M", localtime())
+    return f'log-{current_date}.txt'
+
+
+def get_encoded_photo_by_name(filename):
+    full_path = path.join(PHOTOS_PATH, filename)
+    photo = face_recognition.load_image_file(full_path)
+    return face_recognition.face_encodings(photo)[0]
+
+
+def get_random_encoded_photo_not_in(people):
+    random_photo = None
+
+    people_photos = map(lambda person: person['photos'], people)
+
+    while not random_photo:
+        photo = random.choice(PEOPLE)
+        if photo not in people_photos:
+            random_photo = photo
+
+    return get_encoded_photo_by_name(random_photo)
